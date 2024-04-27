219614f6f8ddf6127212aab3021ca6d404842959
xgolis <xgolis@stuba.sk>
2024-04-27 13:08:50 +0200

    Add mongodb to compose

--- scripts/run.sh
+++ scripts/run.sh
@@ -3,29 +3,39 @@
 # Assign the first command line argument to the variable 'command'
 command=$1
 
-# If no command is provided, default to "start"
-if [ -z "$command" ]; then
-    command="start"
-fi
-
 # Define the project root directory relative to the script location
 ProjectRoot="$(dirname "$0")/.."
 
 # Set environment variables
-export AMBULANCE_API_ENVIRONMENT="Development"
 export AMBULANCE_API_PORT="8080"
+export AMBULANCE_API_MONGODB_USERNAME="root"
+export AMBULANCE_API_MONGODB_PASSWORD="neUhaDnes"
+
+# Define the 'mongo' function to handle Docker Compose operations
+mongo() {
+    docker compose --file "${ProjectRoot}/deployments/docker-compose/compose.yaml" "$@"
+}
 
 # Execute actions based on the command
 case $command in
-    "start")
-        go run "${ProjectRoot}/cmd/ambulance-api-service"
-        ;;
     "openapi")
         docker run --rm -ti -v "${ProjectRoot}:/local" openapitools/openapi-generator-cli generate -c /local/scripts/generator-cfg.yaml
+        ;;
+    "start")
+        # Try to start MongoDB and run the service, then ensure MongoDB is stopped
+        mongo up --detach
+        if go run "${ProjectRoot}/cmd/ambulance-api-service"; then
+            mongo down
+        else
+            mongo down
+            exit 1
+        fi
+        ;;
+    "mongo")
+        mongo up
         ;;
     *)
         echo "Unknown command: $command"
         exit 1
         ;;
 esac
-
